f959aeecc25544221e139a02b8fe908ab28abb4d
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-08-23 15:52:54 +0200

    Initial commit

---
+++ src/test/java/dev/diego/accommodationbookingservice/AccommodationBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diego.accommodationbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
