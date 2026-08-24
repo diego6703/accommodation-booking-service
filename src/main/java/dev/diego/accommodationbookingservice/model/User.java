9465a71497c5d0c2502f1bba5b7fd2048f987e19
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-08-24 19:29:54 +0200

    feat: add Role and User entity

---
+++ src/main/java/dev/diego/accommodationbookingservice/model/User.java
@@ -0,0 +1,41 @@
+package dev.diego.accommodationbookingservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SoftDelete;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@SoftDelete(columnName = "is_deleted")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
