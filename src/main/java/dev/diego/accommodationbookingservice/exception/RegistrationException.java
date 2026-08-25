f95d9338232d6e8beb482517d1785a680bfd6467
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-08-25 17:30:13 +0200

    feat: add custom RegistrationException

---
+++ src/main/java/dev/diego/accommodationbookingservice/exception/RegistrationException.java
@@ -0,0 +1,7 @@
+package dev.diego.accommodationbookingservice.exception;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
