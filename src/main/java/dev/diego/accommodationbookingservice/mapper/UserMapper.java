1cf5ce806818b8a265f4c21eb4d7d38020a73b6c
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-08-26 16:42:00 +0200

    feat: add UserMapper

--- src/main/java/dev/diego/accommodationbookingservice/mapper/UserMapper.java
+++ src/main/java/dev/diego/accommodationbookingservice/mapper/UserMapper.java
@@ -1,11 +1,15 @@
 package dev.diego.accommodationbookingservice.mapper;
 
 import dev.diego.accommodationbookingservice.config.MapperConfig;
+import dev.diego.accommodationbookingservice.dto.UserResponseDto;
 import dev.diego.accommodationbookingservice.dto.auth.UserRegistrationRequestDto;
-import dev.diego.accommodationbookingservice.dto.auth.UserResponseDto;
+import dev.diego.accommodationbookingservice.dto.user.UserUpdateProfileRequestDto;
 import dev.diego.accommodationbookingservice.model.User;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(config = MapperConfig.class)
 public interface UserMapper {
@@ -14,4 +18,8 @@ public interface UserMapper {
 
     @Mapping(target = "email", expression = "java(requestDto.email().toLowerCase())")
     User toEntity(UserRegistrationRequestDto requestDto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUserFromDto(UserUpdateProfileRequestDto requestDto, @MappingTarget User user);
 }
+
