4f1959fcea37c107ae9b241fd0a4208e6a71d351
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-08-28 18:24:56 +0200

    feat: add PaymentProcessingException

---
+++ src/main/java/dev/diego/accommodationbookingservice/exception/PaymentProcessingException.java
@@ -0,0 +1,12 @@
+package dev.diego.accommodationbookingservice.exception;
+
+public class PaymentProcessingException extends RuntimeException {
+
+    public PaymentProcessingException(String message) {
+        super(message);
+    }
+
+    public PaymentProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
